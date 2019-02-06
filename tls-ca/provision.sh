812b6144c1705fe670eefb0ed8745639d927d1b3
Benjamin Lu <benlumia007@gmail.com>
2019-02-06 01:14:51 -0800

    updated

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -25,5 +25,16 @@ else
 fi
 
 for domain in `get_sites`; do
-    echo "${domain}"
+    if [[ ! -d "/vagrant/certificates/${domain}" ]]; then
+        mkdir -p "/vagrant/certificates/${domain}"
+        cp "/srv/config/certificates/domain.ext" "/vagrant/certificates/${domain}/${domain}.ext"
+        sed -i -e "s/{{DOMAIN}}/${domain}/g" "/vagrant/certificates/${domain}/${domain}.ext"
+
+        noroot openssl genrsa -out "/vagrant/certificates/${domain}/${domain}.key" 4096
+        noroot openssl req -new -key "/vagrant/certificates/${domain}/${domain}.key" -out "/vagrant/certificates/${domain}/${domain}.csr" -subj "/CN=${domain}"
+        noroot openssl x509 -req -in "/vagrant/certificates/${domain}/${domain}.csr" -CA "/vagrant/certificates/ca/ca.crt" -CAkey "/vagrant/certificates/ca/ca.key" -CAcreateserial -out "/vagrant/certificates/${domain}/${domain}.crt" -days 3650 -sha256 -extfile "/vagrant/certificates/${domain}/${domain}.ext"
+        sed -i '/certificate/s/^#//g' "/etc/apache2/sites-available/${domain}.conf"
+    else
+        echo "${domain} has been generated."
+    fi
 done
