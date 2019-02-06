a92664419e063d96972daf8c6b360996d10b4ef3
Benjamin Lu <benlumia007@gmail.com>
2019-02-05 23:27:05 -0800

    updated

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -4,7 +4,7 @@ escaped=`echo ${domain} | sed 's/\./\\\\./g'`
 sandbox_config=/vagrant/sandbox-custom.yml
 
 get_sites() {
-    local value=`cat ${VVV_CONFIG} | shyaml keys sites 2> /dev/null`
+    local value=`cat ${sandbox_config} | shyaml keys sites 2> /dev/null`
     echo ${value:-$@}
 }
 domain=`get_sites`
