c6ba073883aa843385cba90393d687a73e462387
Benjamin Lu <benlumia007@gmail.com>
2019-02-05 23:31:42 -0800

    added root gen

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -7,5 +7,12 @@ get_sites() {
     local value=`cat ${sandbox_config} | shyaml keys sites 2> /dev/null`
     echo ${value:-$@}
 }
-domain=`get_sites`
-echo "${domain}"
+
+if [[ ! -d "/vagrant/certificates/ca" ]]; then
+    noroot mkdir -p "/vagrant/certificates/ca"
+    noroot openssl genrsa -out "/vagrant/certificates/ca/ca.key" 4096
+    noroot openssl req -x509 -new -nodes -key "/vagrant/certificates/ca/ca.key" -sha256 -days 3650 -out "/vagrant/certificates/ca/ca.crt" -subj "/CN=Sandbox Internal CA"
+    a2enmod ssl headers rewrite
+else
+    echo "a root certificate of ca has been generated."
+fi
