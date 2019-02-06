d5ce347c3fd52b89fdb5518f52f35169e92fbb55
Benjamin Lu <benlumia007@gmail.com>
2019-02-06 00:14:18 -0800

    updated

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -13,8 +13,6 @@ get_sites() {
 
 domain=`get_sites`
 
-echo "${domain}"
-
 noroot() {
     sudo -EH -u "vagrant" "$@";
 }
@@ -26,4 +24,17 @@ if [[ ! -d "/vagrant/certificates/ca" ]]; then
     a2enmod ssl headers rewrite
 else
     echo "a root certificate of ca has been generated."
+fi
+
+if [[ ! -d "/vagrant/certificates/${domain}" ]]; then
+    mkdir -p "/vagrant/certificates/${domain}"
+    cp "/srv/config/certificates/domain.ext" "/vagrant/certificates/${domain}/${domain}.ext"
+    sed -i -e "s/{{DOMAIN}}/${domain}/g" "/vagrant/certificates/${domain}/${domain}.ext"
+
+    noroot openssl genrsa -out "/vagrant/certificates/${domain}/${domain}.key" 4096
+    noroot openssl req -new -key "/vagrant/certificates/${domain}/${domain}.key" -out "/vagrant/certificates/${domain}/${domain}.csr" -subj "/CN=${DOMAIN}"
+    noroot openssl x509 -req -in "/vagrant/certificates/${domain}/${domain}.csr" -CA "/vagrant/certificates/ca/ca.crt" -CAkey "/vagrant/certificates/ca/ca.key" -CAcreateserial -out "/vagrant/certificates/${domain}/${domain}.crt" -days 3650 -sha256 -extfile "/vagrant/certificates/${domain}/${domain}.ext"
+    sed -i '/certificate/s/^#//g' /etc/apache2/sites-available/${domain}.conf
+else
+    echo "${domain} has been generated."
 fi
