3da856c099c08056d7c3c0664499756de9b052d8
Benjamin Lu <benlumia007@gmail.com>
2019-02-05 23:37:01 -0800

    updated

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
-escaped=`echo ${domain} | sed 's/\./\\\\./g'`
 sandbox_config=/vagrant/sandbox-custom.yml
 
 get_sites() {
     local value=`cat ${sandbox_config} | shyaml keys sites 2> /dev/null`
     echo ${value:-$@}
+}
+
+noroot() {
+    sudo -EH -u "vagrant" "$@";
 }
 
 domain='get_sites'
