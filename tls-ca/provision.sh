f6cd03a6cdf720dc44aea9e1efad2104d28ba0ef
Benjamin Lu <benlumia007@gmail.com>
2019-02-05 23:09:21 -0800

    test

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -1,1 +1,11 @@
-echo "test"
+#!/bin/bash
+domain=${1}
+escaped=`echo ${domain} | sed 's/\./\\\\./g'`
+sandbox_config="/vagrant/sandbox-custom.yml"
+
+get_config_value() {
+    local value=`cat ${sandbox_config} | shyaml get-value sites.${escaped}.custom.${1} 2> /dev/null`
+    echo ${value:-$2}
+}
+
+echo "${domain}"
