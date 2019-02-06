861dc461167d89f72d633d972622b307d7abe950
Benjamin Lu <benlumia007@gmail.com>
2019-02-06 01:03:33 -0800

    add for statement

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -11,8 +11,6 @@ get_sites() {
     echo ${value:-$@}
 }
 
-domain=`get_sites`
-
 noroot() {
     sudo -EH -u "vagrant" "$@";
 }
@@ -26,17 +24,17 @@ else
     echo "a root certificate of ca has been generated."
 fi
 
-if [[ ! -d "/vagrant/certificates/${domain}" ]]; then
-    mkdir -p "/vagrant/certificates/${domain}"
-    cp "/srv/config/certificates/domain.ext" "/vagrant/certificates/${domain}/${domain}.ext"
-    sed -i -e "s/{{DOMAIN}}/${domain}/g" "/vagrant/certificates/${domain}/${domain}.ext"
+for domain in `get_sites`; do
+    if [[ ! -d "/vagrant/certificates/${domain}" ]]; then
+        mkdir -p "/vagrant/certificates/${domain}"
+        cp "/srv/config/certificates/domain.ext" "/vagrant/certificates/${domain}/${domain}.ext"
+        sed -i -e "s/{{DOMAIN}}/${domain}/g" "/vagrant/certificates/${domain}/${domain}.ext"
 
-    noroot openssl genrsa -out "/vagrant/certificates/${domain}/${domain}.key" 4096
-    noroot openssl req -new -key "/vagrant/certificates/${domain}/${domain}.key" -out "/vagrant/certificates/${domain}/${domain}.csr" -subj "/CN=${DOMAIN}"
-    noroot openssl x509 -req -in "/vagrant/certificates/${domain}/${domain}.csr" -CA "/vagrant/certificates/ca/ca.crt" -CAkey "/vagrant/certificates/ca/ca.key" -CAcreateserial -out "/vagrant/certificates/${domain}/${domain}.crt" -days 3650 -sha256 -extfile "/vagrant/certificates/${domain}/${domain}.ext"
-    sed -i '/certificate/s/^#//g' /etc/apache2/sites-available/${domain}.conf
-else
-    echo "${domain} has been generated."
-fi
-
-echo "${domain"
+        noroot openssl genrsa -out "/vagrant/certificates/${domain}/${domain}.key" 4096
+        noroot openssl req -new -key "/vagrant/certificates/${domain}/${domain}.key" -out "/vagrant/certificates/${domain}/${domain}.csr" -subj "/CN=${DOMAIN}"
+        noroot openssl x509 -req -in "/vagrant/certificates/${domain}/${domain}.csr" -CA "/vagrant/certificates/ca/ca.crt" -CAkey "/vagrant/certificates/ca/ca.key" -CAcreateserial -out "/vagrant/certificates/${domain}/${domain}.crt" -days 3650 -sha256 -extfile "/vagrant/certificates/${domain}/${domain}.ext"
+        sed -i '/certificate/s/^#//g' /etc/apache2/sites-available/${domain}.conf
+    else
+        echo "${domain} has been generated."
+    fi
+done
