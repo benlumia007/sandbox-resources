5128be1d3516a4c5dd86e42da1e4eb373e4e7689
Benjamin Lu <benlumia007@users.noreply.github.com>
2019-02-06 13:25:40 -0800

    Update provision.sh
    removed a2enmod

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -19,7 +19,6 @@ if [[ ! -d "/vagrant/certificates/ca" ]]; then
     noroot mkdir -p "/vagrant/certificates/ca"
     noroot openssl genrsa -out "/vagrant/certificates/ca/ca.key" 4096
     noroot openssl req -x509 -new -nodes -key "/vagrant/certificates/ca/ca.key" -sha256 -days 3650 -out "/vagrant/certificates/ca/ca.crt" -subj "/CN=Sandbox Internal CA"
-    a2enmod ssl headers rewrite
 else
     echo "a root certificate of ca has been generated."
 fi
