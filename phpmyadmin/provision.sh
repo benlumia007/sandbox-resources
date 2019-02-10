78089ea103ded09cecd7ae7e5c22888df96879ff
Benjamin Lu <benlumia007@gmail.com>
2019-02-10 02:15:08 -0800

    update

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -11,4 +11,5 @@ if [[ ! -d "/srv/www/dashboard/public_html/phpmyadmin" ]]; then
     echo "extracting phpMyAdmin-4.8.5-all-languages"
     wget https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.zip -O phpmyadmin.zip
     unzip phpmyadmin.zip -d "/srv/www/dashboard/public_html/phpmyadmin"
+    mv phpMyAdmin-4.8.5-all-languages/* /srv/www/dashboard/public_html/phpmyadmin
 fi
