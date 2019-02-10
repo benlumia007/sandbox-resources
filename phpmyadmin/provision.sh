bc7d07a9adc07cd251f64766a3958af00384f892
Benjamin Lu <benlumia007@gmail.com>
2019-02-10 01:22:55 -0800

    testing

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -6,7 +6,7 @@ noroot() {
 
 if [[ ! -d "/srv/www/dashboard/public_html/phpmyadmin" ]]; then
     echo "creating folder phpmyadmin"
-    mkdir -p "/srv/www/dashboard/public_html/phpmydmin"
+    mkdir -p "/srv/www/dashboard/public_html/phpmyadmin"
     cd "/srv/www/dashboard/public_html/phpmyadmin"
     noroot wget https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.zip -O phpmydmin.zip
     noroot unzip phpmyadmin.zip
