23dc0e6112fb53d5478a4f40ea05f654eb267987
Benjamin Lu <benlumia007@gmail.com>
2019-02-10 02:12:15 -0800

    updated

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -4,12 +4,11 @@ noroot() {
     sudo -EH -u "vagrant" "$@";
 }
 
-if [[ ! -d "/srv/www/dashboard/public_html" ]]; then
+if [[ ! -d "/srv/www/dashboard/public_html/phpmyadmin" ]]; then
     echo "creating folder phpmyadmin"
-    mkdir -p "/srv/www/dashboard/public_html"
+    mkdir -p "/srv/www/dashboard/public_html/phpmyadmin"
     cd "/srv/www/dashboard/public_html/phpmyadmin"
     echo "extracting phpMyAdmin-4.8.5-all-languages"
     wget https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.zip -O phpmyadmin.zip
-    unzip phpmyadmin.zip -d "/srv/www/dashboard/public_html"
-    mv "phpMyAdmin-4.8.5-all-languages" "phpmyadmin"
+    unzip phpmyadmin.zip -d "/srv/www/dashboard/public_html/phpmyadmin"
 fi
