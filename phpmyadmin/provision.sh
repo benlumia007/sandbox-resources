b0c36fd62139e9898ccd1d2160b3404a21cc6454
Benjamin Lu <benlumia007@gmail.com>
2019-02-10 01:39:49 -0800

    testing

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -7,7 +7,7 @@ noroot() {
 if [[ ! -d "/srv/www/dashboard/public_html/phpmyadmin" ]]; then
     echo "creating folder phpmyadmin"
     mkdir -p "/srv/www/dashboard/public_html/phpmyadmin"
-    cd /tmp
+    cd "/srv/www/dashboard/public_html/phpmyadmin"
     echo "extracting phpMyAdmin-4.8.5-all-languages"
     wget https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.zip -O phpmyadmin.zip
     unzip phpmyadmin.zip
