6b5b52cb7ef5e3ab7d2413ca221e03eb0259fe7b
Benjamin Lu <benlumia007@users.noreply.github.com>
2019-05-20 14:43:18 -0700

    Update provision.sh

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -38,11 +38,15 @@ for domain in `get_sites`; do
     fi
 done
 
-  if [[ ! -d "/srv/certificates/dashboard" ]]; then
-      mkdir -p "/srv/certificates/dashboard"
-      cp "/srv/config/certificates/domain.ext" "/srv/certificates/dashboard/dashboard.ext"
-      sed -i -e "s/{{DOMAIN}}/dashboard/g" "/srv/certificates/dashboard/dashboard.ext"
-      noroot openssl genrsa -out "/srv/certificates/dashboard/dashboard.key" 4096
-      noroot openssl req -new -key "/srv/certificates/dashboard/dashboard.key" -out "/srv/certificates/dashboard/dashboard.csr" -subj "/CN=*.dashboard.test"
-      noroot openssl x509 -req -in "/srv/certificates/dashboard/dashboard.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/dashboard/dashboard.crt" -days 3650 -sha256 -extfile "/srv/certificates/dashboard/dashboard.ext"
-  fi
+if [[ ! -d "/srv/certificates/dashboard" ]]; then
+  mkdir -p "/srv/certificates/dashboard"
+  cp "/srv/config/certificates/domain.ext" "/srv/certificates/dashboard/dashboard.ext"
+  sed -i -e "s/{{DOMAIN}}/dashboard/g" "/srv/certificates/dashboard/dashboard.ext"
+  noroot openssl genrsa -out "/srv/certificates/dashboard/dashboard.key" 4096
+  noroot openssl req -new -key "/srv/certificates/dashboard/dashboard.key" -out "/srv/certificates/dashboard/dashboard.csr" -subj "/CN=*.dashboard.test"
+  noroot openssl x509 -req -in "/srv/certificates/dashboard/dashboard.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/dashboard/dashboard.crt" -days 3650 -sha256 -extfile "/srv/certificates/dashboard/dashboard.ext"
+fi
+
+if [[ ! -d "/vagrant/certificates ]]; then
+    ln -s /srv/certificates /vagrant/certificates
+fi
