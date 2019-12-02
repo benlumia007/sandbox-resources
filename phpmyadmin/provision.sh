45cc05d40425d13c10dd948d4e756a2cd1421c04
Benjamin Lu <benlumia007@users.noreply.github.com>
2019-12-02 10:24:07 -0800

    Update phpMyAdmin-4.9.2-all-languages
    phpMyAdmin-4.9.2-all-languages

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -7,10 +7,10 @@ noroot() {
 if [[ ! -d "/srv/www/dashboard/public_html/phpmyadmin" ]]; then
     mkdir -p "/srv/www/dashboard/public_html/phpmyadmin"
     cd "/srv/www/dashboard/public_html/phpmyadmin"
-    echo "extracting phpMyAdmin-4.8.5-all-languages"
-    wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.zip -O phpmyadmin.zip
+    echo "extracting phpMyAdmin-4.9.2-all-languages"
+    wget https://files.phpmyadmin.net/phpMyAdmin/4.9.2/phpMyAdmin-4.9.2-all-languages.zip -O phpmyadmin.zip
     unzip phpmyadmin.zip -d "/srv/www/dashboard/public_html/phpmyadmin"
-    mv phpMyAdmin-4.9.0.1-all-languages/* "/srv/www/dashboard/public_html/phpmyadmin"
-    rm -rf phpMyAdmin-4.9.0.1-all-languages
+    mv phpMyAdmin-4.9.2-all-languages/* "/srv/www/dashboard/public_html/phpmyadmin"
+    rm -rf phpMyAdmin-4.9.2-all-languages
     rm phpmyadmin.zip
 fi
