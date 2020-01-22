766de9403d2c00ac199b487a91cfd4b46ac2aeb0
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-01-22 14:11:46 -0800

    Update provision.sh

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -29,7 +29,7 @@ for domain in `get_sites`; do
 
         noroot openssl genrsa -out "/srv/certificates/${domain}/${domain}.key" 4096
         noroot openssl req -new -key "/srv/certificates/${domain}/${domain}.key" -out "/srv/certificates/${domain}/${domain}.csr" -subj "/CN=*.${domain}.test"
-        noroot openssl x509 -req -in "/srv/certificates/${domain}/${domain}.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/${domain}/${domain}.crt" -days 3650 -sha256 -extfile "/srv/certificates/${domain}/${domain}.ext"
+        noroot openssl x509 -req -in "/srv/certificates/${domain}/${domain}.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/${domain}/${domain}.crt" -days 365 -sha256 -extfile "/srv/certificates/${domain}/${domain}.ext"
         sed -i '/certificate/s/^#//g' "/etc/nginx/conf.d/${domain}.conf"
     fi
 done
@@ -40,5 +40,5 @@ if [[ ! -d "/srv/certificates/dashboard" ]]; then
   sed -i -e "s/{{DOMAIN}}/dashboard/g" "/srv/certificates/dashboard/dashboard.ext"
   noroot openssl genrsa -out "/srv/certificates/dashboard/dashboard.key" 4096
   noroot openssl req -new -key "/srv/certificates/dashboard/dashboard.key" -out "/srv/certificates/dashboard/dashboard.csr" -subj "/CN=*.dashboard.test"
-  noroot openssl x509 -req -in "/srv/certificates/dashboard/dashboard.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/dashboard/dashboard.crt" -days 3650 -sha256 -extfile "/srv/certificates/dashboard/dashboard.ext"
+  noroot openssl x509 -req -in "/srv/certificates/dashboard/dashboard.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/dashboard/dashboard.crt" -days 365 -sha256 -extfile "/srv/certificates/dashboard/dashboard.ext"
 fi
