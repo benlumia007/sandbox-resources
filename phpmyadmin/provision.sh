72a8592f286187c5f9f3489c46a2a92a78bfeed8
benlumia007 <benlumia007@users.noreply.github.com>
2020-04-25 17:53:23 -0700

    Update provision.sh

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -9,7 +9,7 @@ if [[ ! -d "/srv/www/dashboard/public_html/phpmyadmin" ]]; then
     cd "/srv/www/dashboard/public_html/phpmyadmin"
     wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip -O phpmyadmin.zip
     unzip phpmyadmin.zip -d "/srv/www/dashboard/public_html/phpmyadmin"
-    mv phpMyAdmin-4.9.2-all-languages/* "/srv/www/dashboard/public_html/phpmyadmin"
+    mv phpMyAdmin-5.0.2-all-languages/* "/srv/www/dashboard/public_html/phpmyadmin"
     rm -rf phpMyAdmin-5.0.2-all-languages
     rm phpmyadmin.zip
 fi
