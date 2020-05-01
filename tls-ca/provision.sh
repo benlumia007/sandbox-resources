1b04dd446cc95ef99c00c020c24357965a928236
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-05-01 15:19:06 -0700

    output true or false

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -22,14 +22,15 @@ if [[ ! -d "/srv/certificates/ca" ]]; then
 fi
 
 for domain in `get_sites`; do
-    get_provision() {
-      local value=`cat ${sandbox_config} | shyaml keys sites.${domain}.provision 2> /dev/null`
+    get_site_provision() {
+      local value=`cat ${sandbox_config} | shyaml get-value sites.${domain}.provision 2> /dev/null`
       echo ${value:-$@}
     }
 
-    provision = `get_provision`
+    provision=`get_site_provision`
 
     echo "${domain} is set to ${provision}"
+
 
 
     if [[ ! -d "/srv/certificates/${domain}" ]]; then
