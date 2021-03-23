e347231909955eaa89d1d38182f78eee9c1ab6a1
benlumia007 <benlumia007@users.noreply.github.com>
2021-03-23 09:46:09 -0700

    Update provision.sh

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -17,8 +17,8 @@ noroot() {
 
 if [[ ! -d "/srv/certificates/ca" ]]; then
     noroot mkdir -p "/srv/certificates/ca"
-    noroot openssl genrsa -out "/srv/certificates/ca/ca.key" 4096
-    noroot openssl req -x509 -new -nodes -key "/srv/certificates/ca/ca.key" -sha256 -days 365 -out "/srv/certificates/ca/ca.crt" -subj "/CN=Sandbox Internal CA"
+    noroot openssl genrsa -out "/srv/certificates/ca/ca.key" 4096 &>/dev/null
+    noroot openssl req -x509 -new -nodes -key "/srv/certificates/ca/ca.key" -sha256 -days 365 -out "/srv/certificates/ca/ca.crt" -subj "/CN=Sandbox Internal CA" &>/dev/null
 fi
 
 for domain in `get_sites`; do
@@ -47,7 +47,7 @@ if [[ ! -d "/srv/certificates/dashboard" ]]; then
   mkdir -p "/srv/certificates/dashboard"
   cp "/srv/config/certificates/domain.ext" "/srv/certificates/dashboard/dashboard.ext"
   sed -i -e "s/{{DOMAIN}}/dashboard/g" "/srv/certificates/dashboard/dashboard.ext"
-  noroot openssl genrsa -out "/srv/certificates/dashboard/dashboard.key" 4096
-  noroot openssl req -new -key "/srv/certificates/dashboard/dashboard.key" -out "/srv/certificates/dashboard/dashboard.csr" -subj "/CN=*.dashboard.test"
-  noroot openssl x509 -req -in "/srv/certificates/dashboard/dashboard.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/dashboard/dashboard.crt" -days 365 -sha256 -extfile "/srv/certificates/dashboard/dashboard.ext"
+  noroot openssl genrsa -out "/srv/certificates/dashboard/dashboard.key" 4096 &>/dev/null
+  noroot openssl req -new -key "/srv/certificates/dashboard/dashboard.key" -out "/srv/certificates/dashboard/dashboard.csr" -subj "/CN=*.dashboard.test" &>/dev/null
+  noroot openssl x509 -req -in "/srv/certificates/dashboard/dashboard.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/dashboard/dashboard.crt" -days 365 -sha256 -extfile "/srv/certificates/dashboard/dashboard.ext" &>/dev/null
 fi
