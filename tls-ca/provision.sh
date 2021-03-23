0482fe48a24164659333800fae74bcd21f1152b7
benlumia007 <benlumia007@users.noreply.github.com>
2021-03-23 09:44:10 -0700

    Update provision.sh

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -35,9 +35,9 @@ for domain in `get_sites`; do
             cp "/srv/config/certificates/domain.ext" "/srv/certificates/${domain}/${domain}.ext"
             sed -i -e "s/{{DOMAIN}}/${domain}/g" "/srv/certificates/${domain}/${domain}.ext"
 
-            noroot openssl genrsa -out "/srv/certificates/${domain}/${domain}.key" 4096
-            noroot openssl req -new -key "/srv/certificates/${domain}/${domain}.key" -out "/srv/certificates/${domain}/${domain}.csr" -subj "/CN=*.${domain}.test"
-            noroot openssl x509 -req -in "/srv/certificates/${domain}/${domain}.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/${domain}/${domain}.crt" -days 365 -sha256 -extfile "/srv/certificates/${domain}/${domain}.ext"
+            noroot openssl genrsa -out "/srv/certificates/${domain}/${domain}.key" 4096 &>/dev/null
+            noroot openssl req -new -key "/srv/certificates/${domain}/${domain}.key" -out "/srv/certificates/${domain}/${domain}.csr" -subj "/CN=*.${domain}.test" &>/dev/null
+            noroot openssl x509 -req -in "/srv/certificates/${domain}/${domain}.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/${domain}/${domain}.crt" -days 365 -sha256 -extfile "/srv/certificates/${domain}/${domain}.ext" &>/dev/null
             sed -i '/certificate/s/^#//g' "/etc/nginx/conf.d/${domain}.conf"
         fi
     fi
