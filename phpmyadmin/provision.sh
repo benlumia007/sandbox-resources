f53383a45c05b13cbbb8feda76c56c4d827bad91
benlumia007 <benlumia007@users.noreply.github.com>
2021-05-06 10:15:31 -0700

    Update phpmyadmin to 5.1.0

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -7,9 +7,9 @@ noroot() {
 if [[ ! -d "/srv/www/dashboard/public_html/phpmyadmin" ]]; then
     mkdir -p "/srv/www/dashboard/public_html/phpmyadmin"
     cd "/srv/www/dashboard/public_html/phpmyadmin"
-    wget -q https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip -O phpmyadmin.zip
+    wget -q https://files.phpmyadmin.net/phpMyAdmin/5.1.0/phpMyAdmin-5.1.0-all-languages.zip -O phpmyadmin.zip
     unzip -q phpmyadmin.zip -d "/srv/www/dashboard/public_html/phpmyadmin"
-    mv phpMyAdmin-5.0.2-all-languages/* "/srv/www/dashboard/public_html/phpmyadmin"
-    rm -rf phpMyAdmin-5.0.2-all-languages
+    mv phpMyAdmin-5.1.0-all-languages/* "/srv/www/dashboard/public_html/phpmyadmin"
+    rm -rf phpMyAdmin-5.1.0-all-languages
     rm phpmyadmin.zip
 fi
